f2bb7e4a1bcb00f950e21df39767a14a48ba397d
bebob94 <bebo.macis@gmail.com>
2023-02-22 08:47:17 +0100

    add

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -12,40 +12,53 @@ const Cart = () => {
           <strong> Carrello vuoto: </strong> Prego Selezionare qualche prodotto!
         </Alert>
       ) : (
-        <Container>
+        <Container className="mt-4">
           <Row>
-            {products.map((prod, i) => {
-              return (
-                <Col xs={12} className="d-flex m-2 align-items-center" key={i}>
-                  <Button
-                    className="mx-3"
-                    onClick={() =>
-                      dispatch({ type: REMOVE_FROM_CART, payload: i })
-                    }
-                  >
-                    🗑
-                  </Button>
-                  <img
-                    src={prod.thumbnail}
-                    alt={prod.title}
-                    style={{ height: "100px", width: "100px" }}
-                  />
-                  <Row>
-                    <h5 className="mx-3">{prod.title}</h5>
-                    <h5 className="mx-3">{prod.price}€</h5>
-                  </Row>
-                </Col>
-              );
-            })}
+            <Col>
+              <Row>
+                {products.map((prod, i) => {
+                  return (
+                    <Col
+                      xs={6}
+                      className="d-flex m-2 align-items-center"
+                      key={i}
+                    >
+                      <Button
+                        className="mx-3"
+                        onClick={() =>
+                          dispatch({ type: REMOVE_FROM_CART, payload: i })
+                        }
+                      >
+                        🗑
+                      </Button>
+                      <img
+                        src={prod.thumbnail}
+                        alt={prod.title}
+                        style={{ height: "100px", width: "100px" }}
+                      />
+                      <Row>
+                        <h5 className="mx-3">{prod.title}</h5>
+                        <h5 className="mx-3">{prod.price}€</h5>
+                      </Row>
+                    </Col>
+                  );
+                })}
+              </Row>
+            </Col>
+            <Col xs={3}>
+              {" "}
+              <h4 className="mt-4">
+                TOTALE:
+                <strong className="ms-3">
+                  {products.reduce(
+                    (tot, currentvalue) => tot + parseFloat(currentvalue.price),
+                    0
+                  )}{" "}
+                  €
+                </strong>
+              </h4>
+            </Col>
           </Row>
-          <h4 className="mt-4">
-            TOTALE:
-            {products.reduce(
-              (tot, currentvalue) => tot + parseFloat(currentvalue.price),
-              0
-            )}{" "}
-            €
-          </h4>
         </Container>
       )}
     </>
--- src/components/DetailProduct.jsx
+++ src/components/DetailProduct.jsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react";
-import { Button, Card, Container, Row } from "react-bootstrap";
+import { Button, Card, Col, Container, ListGroup, Row } from "react-bootstrap";
 import { useDispatch } from "react-redux";
 import { useParams } from "react-router-dom";
 import { ADD_TO_CART } from "../redux/action/action";
@@ -32,27 +32,33 @@ const DetailProduct = () => {
 
   return (
     <Container>
-      <Row>
-        <Card className="mt-3">
+      <Row className="mt-5">
+        <Col>
           <Card.Img
             variant="top"
             src={product.thumbnail}
-            style={{ height: "500px" }}
+            style={{ height: "350px" }}
           />
-          <Card.Body>
-            <Card.Title>{product.title}</Card.Title>
-            <Card.Text>{product.brand}</Card.Text>
-            <Card.Text>{product.category}</Card.Text>
-            <Card.Text>{product.description}</Card.Text>
-            <Card.Text>{product.price} €</Card.Text>
-            <Button
-              variant="primary"
-              onClick={() => dispatch({ type: ADD_TO_CART, payload: product })}
-            >
-              Aggiungi al carrello 🛒
-            </Button>
-          </Card.Body>
-        </Card>
+        </Col>
+        <Col>
+          <ListGroup className="mt-3">
+            <ListGroup.Item>
+              <strong>{product.title}</strong>
+            </ListGroup.Item>
+            <ListGroup.Item>{product.brand}</ListGroup.Item>
+            <ListGroup.Item>{product.category}</ListGroup.Item>
+            <ListGroup.Item>{product.description}</ListGroup.Item>
+            <ListGroup.Item>{product.price} €</ListGroup.Item>
+          </ListGroup>
+
+          <Button
+            className="mt-4"
+            variant="primary"
+            onClick={() => dispatch({ type: ADD_TO_CART, payload: product })}
+          >
+            Aggiungi al carrello 🛒
+          </Button>
+        </Col>
       </Row>
     </Container>
   );
